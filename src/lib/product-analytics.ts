0b930218ebf490bb4e3fce3964fd5a9c26ccb3c0
gaopengbin <1804287165@qq.com>
2026-08-20 14:28:21 +0800

    refactor: use platform analytics endpoint

--- src/lib/product-analytics.ts
+++ src/lib/product-analytics.ts
@@ -1,6 +1,6 @@
 const endpoint = import.meta.env.VITE_PRODUCT_ANALYTICS_ENDPOINT ||
   (window.location.hostname === 'wallpaper.gpb.cc'
-    ? 'https://laogao.xyz/geod-telemetry/v1/product-events'
+    ? 'https://laogao.xyz/platform-api/v1/product-events'
     : '')
 
 const visitorStorageKey = 'wallpaper-web:analytics-visitor'
